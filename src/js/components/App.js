2ccd71a2ff6ea8a98fbc55c1dc8a3923826f3899
Jack Rans <jwrans@gmail.com>
2015-11-02 17:25:30 +0000

    today's work, needs neatening upbut some examples of state being changed and event handlers

--- src/js/components/App.js
+++ src/js/components/App.js
@@ -1,12 +1,46 @@
 var React = require('react');
 
+var Header = require('./Header.js');
+var List   = require('./List.js');
+var Footer = require('./Footer.js');
+
 var App = React.createClass({
 
+  getInitialState: function () {
+    return {
+      items: ['Apple', 'Egg']
+    }
+  },
+
+  clearFridge: function () {
+    this.setState({items:[]});
+  },
+
+  addMoreDrinks: function () {
+    var items       = this.state.items;
+    var drinkyItems = items.concat(['Dr. Pepper','Fanta Icy Lemon', 'Irn Bru', 'GrapeDrank']);
+
+    this.setState({items: drinkyItems});
+  },
+
+  addMoreDrinksOrFood: function (e) {
+    var items       = this.state.items;
+    var eatyItems = items.concat([e.target.value]);
+
+    if (e.keyCode===13) {
+      this.setState({items: eatyItems});
+    }
+
+  },
+
   render: function () {
-    return(
-      <div>
-        <h1>Hey Fac6</h1>
-        <p>Git clone https://github.com/jrans/React-Webpack-Set-Up</p>
+
+    return (
+      <div className='app-container'>
+        <Header doubleClickHandler={this.addMoreDrinks} title='Fridge' />
+        <input type='text' onKeyDown={this.addMoreDrinksOrFood} />
+        <List items={this.state.items}/>
+        <Footer whatToDoWhenTheFooterIsClicked={this.clearFridge} text='Footer' />
       </div>
     );
   }
--- src/styles/main.js
+++ src/styles/main.js
@@ -1,3 +1,6 @@
 'use strict';
 
 require('./components/App.css');
+require('./components/Header.css');
+require('./components/List.css');
+require('./components/Footer.css');
